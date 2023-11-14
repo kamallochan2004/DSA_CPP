ee60cc8640e7a81bf111acded86d8c36500fe856
kamallochan2004 <theultimatesource2019@gmail.com>
2023-11-14 10:59:56 +0530

    created nCr finder

---
+++ functions/nCr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact*=i;
+    }
+    return fact;
+}
+int nCr(int n,int r){
+     int ans=factorial(n)/(factorial(r)*factorial(n-r));
+     return ans;
+}
+int main(){
+    int n,r;
+    cout<<"Enter value of n"<<endl;
+    cin>>n;
+    cout<<"Enter value of r"<<endl;
+    cin>>r;
+    cout<<"The answer for the question is " << nCr(n,r) << endl;
+}
